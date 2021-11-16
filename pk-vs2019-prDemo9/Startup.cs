54e917123e98e955c66f6e10163c78584357b432
siva0662 <sivakrishna9970@gmail.com>
2021-11-16 15:42:17 +0530

    welcome to the world added

--- pk-vs2019-prDemo9/Startup.cs
+++ pk-vs2019-prDemo9/Startup.cs
@@ -33,6 +33,7 @@ namespace pk_vs2019_prDemo9
                 endpoints.MapGet("/", async context =>
                 {
                     await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("welcome to the world!");
                 });
             });
         }
